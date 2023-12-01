4b193366cba5733bf628e5d28c845435fd67f542
PlantPalFynn <PlantPalFynn@gmail.com>
2023-12-01 10:39:07 +0100

    finish day 1

--- AoC2023/day1.py
+++ AoC2023/day1.py
@@ -1,15 +1,17 @@
 import AoCparse
 import re
 def replace(s):
-        s = s.replace("one", str(1))
-        s = s.replace("two", str(2))
-        s = s.replace("three", str(3))
-        s = s.replace("four", str(4))
-        s = s.replace("five", str(5))
-        s = s.replace("six", str(6))
-        s = s.replace("seven", str(7))
-        s = s.replace("eight", str(8))
-        s = s.replace("nine", str(9))
+        s = s.replace("nine", "nine"+str(9)+"nine")
+        s = s.replace("eight", "eigth"+str(8)+"eight")
+        s = s.replace("seven", "seven"+str(7) + "seven")
+        s = s.replace("six", "six"+str(6)+ "six")
+        s = s.replace("five", "five"+str(5)+"five")
+        s = s.replace("four", "four"+str(4)+"four")
+        s = s.replace("two", "two"+str(2)+"two")
+        s = s.replace("three", "three"+str(3)+"three")
+        s = s.replace("one", "one"+str(1)+"one")
+        
+        
         return s
 
 input = AoCparse.getinput(1)
