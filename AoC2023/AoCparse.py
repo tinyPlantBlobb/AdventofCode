fcd0f25e70dcc76baa63b0c7afe4934e65ca5408
PlantPalFynn <PlantPalFynn@gmail.com>
2023-12-01 10:00:48 +0100

    fix input

--- AoC2023/AoCparse.py
+++ AoC2023/AoCparse.py
@@ -1,6 +1,6 @@
 
 def getinput(day):
-    path="D:\Code\\advent of code\\2023\\input-Day"+day
+    path="D:\Code\\advent of code\\2023\\input-Day"+str(day)
     f = open(path, "r")
     t = f.readlines()
     return t
--- AoC2023/day1.py
+++ AoC2023/day1.py
@@ -1,9 +1,12 @@
-import AoC2023.AoCparse
+import AoCparse
 import re
 
 input = AoCparse.getinput(1)
 result = 0
 for s in input:
         nbrs=re.findAll("\d",s)
-        line = ""+nbrs[0] + nbrs[-1]
+        line = str(nbrs[0]) + str(nbrs[-1])
+        print(line)
         result += int(line)
+
+print(result)
