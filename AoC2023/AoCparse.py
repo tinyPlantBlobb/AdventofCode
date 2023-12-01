68223e70db8ce5019159ee077fae6dcccf4846ef
PlantPalFynn <PlantPalFynn@gmail.com>
2023-12-01 10:28:07 +0100

    solve for part one and start of pt 2

--- AoC2023/AoCparse.py
+++ AoC2023/AoCparse.py
@@ -1,6 +1,6 @@
 
 def getinput(day):
-    path="D:\Code\\advent of code\\2023\\input-Day"+str(day)
+    path="D:\Code\\advent of code\\AoC2023\\input-Day"+str(day)+ ".txt"
     f = open(path, "r")
     t = f.readlines()
     return t
--- AoC2023/day1.py
+++ AoC2023/day1.py
@@ -1,10 +1,24 @@
 import AoCparse
 import re
+def replace(s):
+        s = s.replace("one", str(1))
+        s = s.replace("two", str(2))
+        s = s.replace("three", str(3))
+        s = s.replace("four", str(4))
+        s = s.replace("five", str(5))
+        s = s.replace("six", str(6))
+        s = s.replace("seven", str(7))
+        s = s.replace("eight", str(8))
+        s = s.replace("nine", str(9))
+        return s
 
 input = AoCparse.getinput(1)
 result = 0
+        
 for s in input:
-        nbrs=re.findAll("\d",s)
+        t = replace(s)
+        print(t)
+        nbrs=re.findall("\d",t)
         line = str(nbrs[0]) + str(nbrs[-1])
         print(line)
         result += int(line)
