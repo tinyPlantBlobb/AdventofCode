9b56b2f1a27a01501f740ffb3f461171d11edf11
plantpalfynn <PlantPalFynn@gmail.com>
2023-12-03 20:10:09 +0100

    day 3

--- AoC2023/AoCparse.py
+++ AoC2023/AoCparse.py
@@ -2,7 +2,7 @@
 def getinput(day):
     baselp = "C:\\Users\\isabe\\Code\\AdventofCode\\AoC2023"
     basetw="D:\Code\\advent of code\\AoC2023"
-    path = baselp + "\\input-Day" + str(day)+ ".txt"
+    path = basetw + "\\input-Day" + str(day)+ ".txt"
     f = open(path, "r")
-    t = f.readlines()
+    t = [l.strip() for l in f.readlines()]
     return t
--- AoC2023/day3.py
+++ AoC2023/day3.py
@@ -1,30 +1,49 @@
 import AoCparse
-import re
-from string import punctuation
+from collections import defaultdict
 
-def addat(li, str, index):
-    li[index:index]= [str]
-    return li
+lines = AoCparse.getinput(3)
+print(lines)
 
-input = AoCparse.getinput(3)
-print(input)
-i = 0
-j=0
-specialchar= "[!\"#$%&'()*+,-\\/:;<=>?@[\]^_`{|}~]"
-for l in input:
-    characterindex = re.findall(specialchar,l)
-    l = l.split(punctuation)
-    print(l)
-    j=0
-    for k in l:
-        if len(str(k)) > 1:
-            for o in range(len(str(k))):
-                addat(l, k, j)
-            j+=(len(k)-1)
+# create a mapping between each number and it's coordinate
+mapping = defaultdict(str)
+for ln, line in enumerate(lines):
+  row = 0
+  while row < len(line):
+    char = line[row]
+    if char.isdigit():
+      num = ""
+      track = []
+      while row < len(line) and line[row].isdigit():
+        num += line[row]
+        track.append(f"{str(ln)}_{str(row)}")
+        row += 1
+      for t in track:
+        mapping[t] = num
+    row += 1
+print(mapping)
 
-        j+=1
-    i+=1
-    #print(characterindex)
-    #print(sep)
-print(input)
+p1 = 0
+p2 = 0
+# go over each symbol, and if a number matches, 'visit' it's mapped number
+for ln, line in enumerate(lines):
+  for row, char in enumerate(line):
+    # check a symbol
+    if char != "." and not char.isdigit():
+      values = []
+      for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+          obj = lines[ln + x][row + y]
+          if obj.isdigit():
+            position = f"{ln + x}_{row + y}"
+            num = mapping[position]
+            # record numbers around this symbol
+            if int(num) not in values:
+              values.append(int(num))
+              p1 += int(num)
+      # check * gear, for part 2
+      if char == "*" and len(values) == 2:
+        p2 += values[0] * values[1]
 
+print("answer of part 1:" + str(p1))
+print("answer of part 2:" + str(p2))
+# tbh this one hurt my head too much trying to parse everything so I gave up
