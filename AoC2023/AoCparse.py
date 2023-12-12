5f166215aa9681c04c058e03676b12598a09fe49
plantpalfynn <PlantPalFynn@gmail.com>
2023-12-12 23:06:21 +0100

    fix A0cpath

--- AoC2023/AoCparse.py
+++ AoC2023/AoCparse.py
@@ -1,8 +1,7 @@
 
 def getinput(day):
-    baselp = "C:\\Users\\isabe\\Code\\AdventofCode\\AoC2023"
-    basetw="D:\Code\\advent of code\\AoC2023"
-    path = baselp + "\\input-Day" + str(day)+ ".txt"
+    base = "AoC2023"
+    path = base + "\\input-Day" + str(day)+ ".txt"
     f = open(path, "r")
     t = [l.strip() for l in f.readlines()]
     return t
