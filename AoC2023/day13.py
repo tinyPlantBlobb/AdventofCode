06a100d644f9c9adaa6711acf3bd6835c7e52d6c
plantpalfynn <PlantPalFynn@gmail.com>
2023-12-17 18:29:51 +0100

    unfinished day 17 and inputs

--- AoC2023/day13.py
+++ AoC2023/day13.py
@@ -16,13 +16,33 @@ def findmirror(area):
                 break
             if area[y[0]-(len(area)-i)] == area[i]:
                 if checkotherlines(area, y[0]): 
-                    mirrorv = min(y[0], mirrorv)
+                    return min(y[0], mirrorv)
                 print(f"{area[i]} and {area[y[0]-(len(area)-i)]} in {i} and {y[0]-(len(area)-i)}")
                 print(f"mirror at {mirrorv} in line {y[0]}")
-    return mirrorv
+    for x in enumerate(area[0]):
+        for i in range(x[0], len(area)):
+            if x[0]-(len(area[0])-i) < 0:
+                break
+            if area[x[0]-(len(area)-i)] == area[i]:
+                if checkotherlines(area, x[0]): 
+                    return min(x[0], mirrorh)
+                print(f"{area[i]} and {area[x[0]-(len(area)-i)]} in {i} and {x[0]-(len(area)-i)}")
+                print(f"mirror at {mirrorh} in line {x[0]}")
+    return mirrorh
 
 
 def checkotherlines(area, index):
+    if index > len(area)/2:
+        for i in range(0,len(area) -index ,1):
+            if area[index +i] != area[index-1-i]:
+                return False
+    else:
+        for i in range(0, index, 1):
+            if area[index +1+i] != area[index-i]:
+                return False
+    return True
+            
+            
     return True
 
 areas= [list(g) for k, g in groupby(input, key=bool) if k]
