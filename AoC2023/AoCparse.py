ec984f210a5542e23ff4a9037c4fb7331491fef0
plantpalfynn <PlantPalFynn@gmail.com>
2023-12-18 08:23:47 +0100

    day 18

--- AoC2023/AoCparse.py
+++ AoC2023/AoCparse.py
@@ -6,3 +6,11 @@ def getinput(day):
     t = [l.strip() for l in f.readlines()]
     f.close()
     return t
+
+def tofile(board):
+    output = open("AoC2023\\output.txt", "w")
+    for each in board:
+        output.write("".join(each))
+        output.write("\n")
+    output.close()
+    
