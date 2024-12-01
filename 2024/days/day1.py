f8f7d3063d62b76215bf54eea4468980e8baf5e5
plantpalfynn <plantpalfynn@gmail.com>
2024-12-01 16:28:15 +0100

    finsh day 1, add puzzle input

--- 2024/days/day1.py
+++ 2024/days/day1.py
@@ -1,9 +1,10 @@
-print("Day 1 of Advent of Code!")
-
-
 def run(input):
+    print("Day 1 of Advent of Code!")
     lists = [i.split("   ") for i in input]
     list1 = sorted([int(i[0]) for i in lists])
     list2 = sorted([int(i[1]) for i in lists])
     result = sum(list(map(lambda x: abs(x[0] - x[1]), zip(list1, list2))))
     print(result)
+    l2mapping = {i: list2.count(i) for i in list2}
+    result2 = sum(list(map(lambda x: x * l2mapping.get(x, 0), list1)))
+    print(result2)
