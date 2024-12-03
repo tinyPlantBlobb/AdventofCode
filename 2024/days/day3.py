3430163dca36d09a0fa00414227588d3a4cd92e3
plantpalfynn <plantpalfynn@gmail.com>
2024-12-03 10:22:31 +0100

    solution for pt 1 and puzzle input

--- 2024/days/day3.py
+++ 2024/days/day3.py
@@ -1,3 +1,29 @@
+import re
+from operator import mul
+
+
 def run(input):
-    print(input)
-    print(input)
+    multiplications = list(
+        map(lambda x: re.findall(r"mul\(\d{1,3},\d{1,3}\)", x.strip()), input)
+    )
+    print(multiplications)
+    print()
+    result = sum(
+        list(
+            map(
+                lambda y: sum(
+                    list(
+                        map(
+                            lambda x: int(re.findall(r"\d+", x)[0])
+                            * int(re.findall(r"\d+", x)[1])
+                            if x
+                            else 0,
+                            y,
+                        ),
+                    )
+                ),
+                multiplications,
+            )
+        )
+    )
+    print(result)
