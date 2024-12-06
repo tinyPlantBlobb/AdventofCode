158dc38f172ea6125a372d9d09350346319b31a3
plantpalfynn <plantpalfynn@gmail.com>
2024-12-06 12:36:44 +0100

    start of pt 1

--- 2024/days/day6.py
+++ 2024/days/day6.py
@@ -1,3 +1,28 @@
 def run(input):
-    print(input)
-    print(input)
+    guardpos = [
+        (x, y)
+        for y in range(len(input))
+        for x in range(len(input[y]))
+        if input[y][x] in "^v<>"
+    ][0]
+    guards = (guardpos, translate_direction(input[guardpos[1]][guardpos[0]]))
+    obstacles = get_obstacles(input)
+    print(guards, obstacles)
+
+
+def get_obstacles(input):
+    return [
+        (int(x), int(y))
+        for y in range(len(input))
+        for x in range(len(input[y]))
+        if input[y][x] == "#"
+    ]
+
+
+def translate_direction(char):
+    return {
+        "v": (0, 1),
+        "^": (0, -1),
+        "<": (-1, 0),
+        ">": (1, 0),
+    }[char]
