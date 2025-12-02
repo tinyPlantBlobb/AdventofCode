b8f0a95546aa0315af865236c25a3dc2ebe62fcb
plantpalfynn <plantpalfynn@gmail.com>
2025-12-02 12:56:23 +0100

    add day 1 and 2 base and basic input readingw

--- 2025/AoC2025/src/main.rs
+++ 2025/AoC2025/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs;
+pub mod day1; pub mod day2;pub mod day3;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day :String = args[1].clone();
+    let mut puzzle_file : String = String::from("day");
+    puzzle_file.push_str(&day);
+    if args.len() > 2{
+        if &args[2] == "test"{
+         puzzle_file.push_str("_test");
+        }
+    }
+
+    let puzzle_input= fs::read_to_string(puzzle_file).expect("could not read file");
+    println!("puzzle input is {}", puzzle_input);
+    match day.as_str() {
+        "1"=> day1::run(puzzle_input),
+        "2"=> day2::run(puzzle_input),
+        _=>println!("no code found"),
+    }
 }
