51f02b02c473d3d59a5cd7cdda20b94730305c71
plantpalfynn <plantpalfynn@gmail.com>
2025-12-03 14:39:15 +0100

    update main

--- 2025/AoC2025/src/main.rs
+++ 2025/AoC2025/src/main.rs
@@ -4,16 +4,14 @@ pub mod day1; pub mod day2;pub mod day3;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let day :String = args[1].clone();
-    let mut puzzle_file : String = String::from("day");
+    let mut puzzle_file : String = String::from("input/day");
     puzzle_file.push_str(&day);
     if args.len() > 2{
         if &args[2] == "test"{
          puzzle_file.push_str("_test");
         }
     }
-
-    let puzzle_input= fs::read_to_string(puzzle_file).expect("could not read file");
-    println!("puzzle input is {}", puzzle_input);
+    let puzzle_input= fs::read_to_string(puzzle_file).expect("could not read file ");
     match day.as_str() {
         "1"=> day1::run(puzzle_input),
         "2"=> day2::run(puzzle_input),
