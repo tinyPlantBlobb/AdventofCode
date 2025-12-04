15479311f880fbbb15a436d059ba7bc6c1800d02
plantpalfynn <plantpalfynn@gmail.com>
2025-12-04 15:58:32 +0100

    clean up day 1 code

--- 2025/AoC2025/src/day1.rs
+++ 2025/AoC2025/src/day1.rs
@@ -17,33 +17,22 @@ pub fn run(input:String){
             let mut position_between : i64= (position + direction * turn);
             let overflow_turns = i64::abs(turn.div_euclid(100));
             position = position_between.rem_euclid(100); 
-            //print!("{}  , {}  ", position, position_between);
             if position == 0 {
-            result1+=1;
-            if overflow_turns >0 {
+                result1+=1;
+                if overflow_turns >0 {
                     result2+=i64::abs(overflow_turns);
-                    //println!("pos 0 and overflow");
-                    result2+=1;
-            } else {
+                }
                 result2+=1;
-                 //println!("pos 0 and no overflow");
-            }
             } else {
             if overflow_turns >0 {
-                //println!("pos diff and overflow");
-                    result2+=i64::abs(overflow_turns);
-                    position_between -= direction *overflow_turns * 100;
-                    if  ((position_between > 100 )|| (position_between < 0 && prev_position != 0)){
+                result2+=i64::abs(overflow_turns);
+                position_between -= direction *overflow_turns * 100;}
+                if  ((position_between > 100 )|| (position_between < 0 && prev_position != 0)){
                     result2 += 1; 
-                    }
-            }else {
-                //println!("pos diff and no overflow");
-            if  ((position_between > 100 )|| (position_between < 0 && prev_position != 0)) {
-                result2+=1;
+                }
+
             }
-        }
-            }
-        println!("start {} turn {}  pre mod {} post mod {} div {} result {}", prev_position,turn, position_between, position,overflow_turns, result2);
+        //println!("start {} turn {}  pre mod {} post mod {} div {} result {}", prev_position,turn, position_between, position,overflow_turns, result2);
         }
 
     }
