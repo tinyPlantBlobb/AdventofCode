a07aa479a13106b8204cba3abce6ec7f54309e61
plantpalfynn <plantpalfynn@gmail.com>
2025-12-08 19:23:21 +0100

    finish day 3 pt 1

--- 2025/AoC2025/src/day3.rs
+++ 2025/AoC2025/src/day3.rs
@@ -1,28 +1,33 @@
 pub fn run(input: String) {
     let battery_lines = input.split_whitespace();
-    println!("{:?}", battery_lines);
+    println!("full line {:?}", battery_lines);
+    println!("joltage sum {}", part1(battery_lines));
+}
+fn part1(battery_lines: std::str::SplitWhitespace)->i64{
     let mut joltage_sum: i64 = 0;
     for i in battery_lines {
         let mut result = String::from("");
-        let largest_char = largest_first(i.to_string());
-        let remainder = String::from(i);
-        String::from(i).remove(largest_char.1);
+        let mut start_input = i.to_string();
+        start_input.split_off(i.len()-1);
+        let largest_char = largest_first(start_input);
+        let mut remainder = String::from(i);
+        remainder=remainder.split_off(largest_char.1+1);
         let second = largest_first(remainder);
-        if largest_char.1 > second.1 {
-            result.push(second.0);
-            result.push(largest_char.0);
-        } else {
+       
             result.push(largest_char.0);
             result.push(second.0);
-        }
-        println!("{}, {:?} , {:?} ", result, largest_char, second);
+        
         joltage_sum += i64::from_str_radix(&result, 10).expect("no number");
     }
-    println!("{}", joltage_sum);
+
+    return joltage_sum;
+}
+
+fn part2(battery_lines: std::str::SplitWhitespace)->i64{
+
 }
 
 fn largest_first(input: String) -> (char, usize) {
-    println!("{}", input);
     let mut largest = ('0', 0);
     for c in input.chars().enumerate() {
         if c.1 > largest.0 {
@@ -31,3 +36,4 @@ fn largest_first(input: String) -> (char, usize) {
     }
     return (largest.0, largest.1);
 }
+
